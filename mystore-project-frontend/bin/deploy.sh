f9eb1f16513a3af03d6b1d1bd6671244735fe47b
markyMARK <>
2021-11-05 19:52:49 -0400

    fix:  Update build path to deploy to S3

--- mystore-project-frontend/bin/deploy.sh
+++ mystore-project-frontend/bin/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 cp --recursive --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./dist s3://mystorefrontend-bucket/
+aws s3 cp --recursive --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./dist/mystore-frontend s3://mystorefrontend-bucket/
