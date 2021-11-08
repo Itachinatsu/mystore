fa8e05b75b01b6b969630fbe992a14fcfdc6c872
markyMARK <>
2021-11-08 00:47:51 -0500

    feat: Use xvfb

--- mystore-project-frontend/bin/test-e2e.sh
+++ mystore-project-frontend/bin/test-e2e.sh
@@ -1,4 +1,4 @@
 sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 
-sudo xvfb-run npx cypress verify
-sudo xvfb-run npx cypress run
+sudo xvfb-run cypress verify
+sudo xvfb-run cypress run
