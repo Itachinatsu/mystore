48d5b61d3047f5d6b2cddef6b0d0bbe6677dfb60
markyMARK <>
2021-11-08 02:47:31 -0500

    feat:  Updated e2e test script

--- mystore-project-frontend/bin/test-e2e.sh
+++ mystore-project-frontend/bin/test-e2e.sh
@@ -1,2 +1,2 @@
 sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
-sudo xvfb-run ng run mystore-frontend:cypress-run
+sudo xvfb-run npx cypress run --headless
