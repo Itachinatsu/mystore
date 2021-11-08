f3507f935bdc222863eab6dcf114be44b5372ee7
markyMARK <>
2021-11-08 01:31:04 -0500

    feat:  Update workflow

--- mystore-project-frontend/bin/test-e2e.sh
+++ mystore-project-frontend/bin/test-e2e.sh
@@ -1,2 +1,2 @@
-apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
-xvfb-run ng run mystore-frontend:cypress-run
+sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+sudo xvfb-run ng run mystore-frontend:cypress-run
