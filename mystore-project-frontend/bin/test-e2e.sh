cad000eca63eec2b9b29a2dafb02be7735bb85a5
markyMARK <>
2021-11-08 01:17:24 -0500

    feat:  use cypress orb

--- mystore-project-frontend/bin/test-e2e.sh
+++ mystore-project-frontend/bin/test-e2e.sh
@@ -1,4 +1,2 @@
-sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
-
-sudo xvfb-run cypress verify
-sudo xvfb-run cypress run
+apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+xvfb-run ng run mystore-frontend:cypress-run
