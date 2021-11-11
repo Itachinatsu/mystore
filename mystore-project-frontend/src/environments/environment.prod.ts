b18d279e2cf88f10e5e4320dedc08d83fb8c99a9
markyMARK <>
2021-11-11 07:50:45 -0500

    fix: Update deploy-staging scripts

--- mystore-project-frontend/src/environments/environment.prod.ts
+++ mystore-project-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://mystorebackend-env-2.eba-upfexrmm.us-east-1.elasticbeanstalk.com' // define the production environment API URL
+  apiUrl: 'http://Mystorebackend-env.eba-jydy275n.us-east-1.elasticbeanstalk.com' // define the production environment API URL
 };
